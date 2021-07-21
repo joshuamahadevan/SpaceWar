b498d7bac12e7da112f706f853920a7092a55238
joshuamahadevan <joshuamahadevan23@gmail.com>
2021-07-22 00:58:41 +0530

    created basic player with movements

---
+++ app.js
@@ -0,0 +1,110 @@
+const canvas=document.getElementById("canvas")
+const c=canvas.getContext("2d")
+
+c.canvas.background="rgba(50,50,50,.8)"
+
+function resize(){
+    c.canvas.width=innerWidth-5;
+    c.canvas.height=innerHeight-5;
+}
+
+resize()
+
+addEventListener("resize", resize);
+
+addEventListener("mousemove", (e) => {
+    player.mx=e.clientX;
+    player.my=e.clientY;
+})
+
+
+class Player{
+    constructor(){
+        this.x=innerWidth/2;
+        this.y=innerHeight/2;
+        this.size=100;
+        this.controls=[];
+        this.speed=5;
+        this.angle=0;
+        this.mx=this.x;
+        this.my=this.y;
+    }
+    draw(){
+        this.update()
+        const img=new Image()
+        img.src="svgs/player.svg"
+        c.save()
+        c.translate(this.x,this.y)
+        c.rotate(this.angle)
+        c.drawImage(img,-this.size/2, -this.size/2, this.size,this.size)
+        c.restore();
+    }
+    update(){
+        this.angle=Math.atan2(this.my-this.y, this.mx-this.x) + Math.PI/2;
+
+        if( this.controls.includes("left") ){
+            this.x-=this.speed;
+        }
+        if( this.controls.includes("right") ){
+            this.x+=this.speed;
+        }
+        if( this.controls.includes("up") ){
+            this.y-=this.speed;
+        }
+        if( this.controls.includes("down") ){
+            this.y+=this.speed;
+        }
+    }
+}
+
+var player=new Player();
+
+addEventListener("keydown", (e)=>{
+    if(e.key=="a" || e.key=="ArrowLeft"){
+        if( !player.controls.includes("left") ){
+            player.controls.push("left")
+        }
+    }else if(e.key=="w" || e.key=="ArrowUp"){
+        if( !player.controls.includes("up") ){
+            player.controls.push("up")
+        }
+    }else if(e.key=="s" || e.key=="ArrowDown"){
+        if( !player.controls.includes("down") ){
+            player.controls.push("down")
+        }
+    }else if(e.key=="d" || e.key=="ArrowRight"){
+        if( !player.controls.includes("right") ){
+            player.controls.push("right")
+        }
+    }
+})
+
+addEventListener("keyup", (e)=>{
+    if(e.key=="a" || e.key=="ArrowLeft"){
+        if( player.controls.includes("left") ){
+            player.controls.splice(player.controls.findIndex( (e) => e=="left" ),1)
+        }
+    }else if(e.key=="w" || e.key=="ArrowUp"){
+        if( player.controls.includes("up") ){
+            player.controls.splice(player.controls.findIndex((e) => e=="up"),1)
+        }
+    }else if(e.key=="s" || e.key=="ArrowDown"){
+        if( player.controls.includes("down") ){
+            player.controls.splice(player.controls.findIndex((e) => e=="down"),1)
+        }
+    }else if(e.key=="d" || e.key=="ArrowRight"){
+        if( player.controls.includes("right") ){
+            player.controls.splice(player.controls.findIndex((e) => e=="right"),1)
+        }
+    }
+})
+
+
+function play(){
+    requestAnimationFrame(play);
+    c.clearRect(0,0,innerWidth,innerHeight)
+
+    player.draw()
+}
+
+play()
